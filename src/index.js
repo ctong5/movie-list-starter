513eadf5aaf0f6a6a4a0a5980617a12b0bb4167e
ctong5 <cassandra.tong5@gmail.com>
2019-06-30 18:52:41 -0700

    Reset all files to original content

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import movies from './data/exampleMovieListData'
 
-console.log(movies);
-
-ReactDOM.render(<App movies={movies} />, document.getElementById('app'));
+ReactDOM.render(<div>Hello, World</div>, document.getElementById('app'));
